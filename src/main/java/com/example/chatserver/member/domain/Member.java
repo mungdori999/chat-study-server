5a7e94cda2237f83fff022cc498f2989383da884
mungdori <mungdori999@gmail.com>
2025-09-15 15:40:06 +0900

    domain

---
+++ src/main/java/com/example/chatserver/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.example.chatserver.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Role role = Role.USER;
+
+}
